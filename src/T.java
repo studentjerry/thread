e7b15b8cad2dc78374c811e393b273f164498e60
jerrycfli <1036620774@qq.com>
2016-01-06 20:53:48 +0800

    add lock test

--- src/T.java
+++ src/T.java
@@ -1,5 +1,54 @@
+import cf.li.LockTest;
+
 /**
  * Created by li on 1/6/16.
  */
 public class T {
+
+    public static void main(String[]  arg){
+      T t=new T();
+      //测试加锁
+      t.runOneHundred();
+    }
+
+    private void runOneHundred(){
+        for(int i=0;i<100;i++){
+          testLock();
+        }
+    }
+
+    private void testLock(){
+        final LockTest lockTest=new LockTest();
+       Thread one= new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lockTest.write();
+            }
+        });
+        one.start();
+       Thread two= new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lockTest.read();
+            }
+        });
+
+        Thread three=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lockTest.write();
+            }
+        });
+        three.start();
+        two.start();
+        try {
+            one.join();
+            two.join();
+            three.join();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
